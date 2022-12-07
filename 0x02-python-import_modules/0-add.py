8b8c99b420fdffad857b74a6b58c1f0008f6360f
Chidinmajessy <111018400+Chidinmajessy@users.noreply.github.com>
2022-12-07 01:18:56 +0100

    Update 0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,4 +1,10 @@
-#0-add.py   
-
-
-
+#!/usr/bin/python3
+def add(a, b):
+    """My addition function
+    Args:
+        a: first integer
+        b: second integer
+    Returns:
+        The return value. a + b
+    """
+    return (a + b)
