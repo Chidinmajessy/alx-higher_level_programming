3533392d89a3d03a8b41f23fac422243dc29a731
Chidinmajessy <111018400+Chidinmajessy@users.noreply.github.com>
2023-03-15 08:20:00 -0700

    Update 101-sorted.js

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const dict = require('./100-data.js').dict;
+const dict = require('./101-data.js').dict;
 const newDict = {};
 for (const key in dict) {
   if (newDict[dict[key]] === undefined) {
